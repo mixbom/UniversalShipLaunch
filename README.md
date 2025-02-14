f391968c4b4651e441043bf45dd1a21228ba1425
mixbom <mixbom6@gmail.com>
2025-02-13 22:57:12 -0500

    Mod Files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # UniversalShipLaunch
-Lethal Company mod that allows any player to launch the ship.
+
+This mod allows any player to launch the ship, bypassing the host-only restriction that was added in a recent update. It’s perfect for multiplayer groups when the host has to go afk and can't be there to launch the ship.
+
+# Info
+
+Please report any bugs to me on Discord or open an issue on my Github!
+
+You can ping me in the Lethal Company Modding Discord Server for any bugs or recommendations.
+
+Discord username: mixbom
---
+++ UniversalShipLaunch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace UniversalShipLaunch
+{
+    [BepInPlugin("com.mixbom.universalshiplaunch", "UniversalShipLaunch", "1.0.0")]
+    public class UniversalShipLaunchPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            var harmony = new Harmony("com.mixbom.universalshiplaunch");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(StartMatchLever))]
+    [HarmonyPatch("Start", MethodType.Normal)]
+    public class StartMatchLeverPatch
+    {
+        static void Postfix(StartMatchLever __instance)
+        {
+            __instance.triggerScript.hoverTip = "[ Click to launch ship ]";
+            __instance.triggerScript.interactable = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(StartMatchLever))]
+    [HarmonyPatch("PullLever", MethodType.Normal)]
+    public class PullLeverPatch
+    {
+        static void Prefix(StartMatchLever __instance)
+        {
+            if (__instance.leverHasBeenPulled)
+            {
+                __instance.StartGame();
+            }
+            else
+            {
+                __instance.EndGame();
+            }
+        }
+    }
+}
